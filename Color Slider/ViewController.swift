7f8fe2309a31a9ee15b60c6cffffb825305b394b
Alex Umarov <umarov.iosdev@gmail.com>
2020-05-22 14:53:02 +0300

    made outlets

--- Color Slider/ViewController.swift
+++ Color Slider/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Color View Outlet
+    @IBOutlet var colorView: UIView!
+    
+    //Color Label Outlet
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    //Color Value Outlet
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    @IBOutlet var blueValue: UILabel!
+    
+    //Sliders Outlet
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
