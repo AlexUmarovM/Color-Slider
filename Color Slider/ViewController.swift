46d942cd659c4779c23971be30fdc97d7ba09b20
Alex Umarov <umarov.iosdev@gmail.com>
2020-05-22 15:30:25 +0300

    add color value from slider value

--- Color Slider/ViewController.swift
+++ Color Slider/ViewController.swift
@@ -31,9 +31,23 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        redValue.text = String(redSlider.value)
+        greenValue.text = String(greenSlider.value)
+        blueValue.text = String(blueSlider.value)
     }
-
-
+   
+    //Color Value Action
+    @IBAction func redSliderAction() {
+        redValue.text = String(format: "%.2f", redSlider.value)
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenValue.text = String(format: "%.2f", greenSlider.value)
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueValue.text = String(format: "%.2f", blueSlider.value)
+    }
+    
 }
 
